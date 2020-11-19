ed270aa6e08fa9e03491a5b80e92b9a72f83b9d3
Ubuntu <ubuntu@ip-172-31-27-212.ec2.internal>
2020-11-19 06:16:53 +0000

    Add home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  
-  resources :books
   devise_for :users
+  root :to => "static_pages#home"
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
